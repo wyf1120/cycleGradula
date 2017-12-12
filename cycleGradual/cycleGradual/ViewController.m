5be1e543df9a59215a038eba3517bcc96f66fbbd
吴宇飞 <wuyufei@wuyufeidemacbook-pro.local>
2017-12-12 17:23:01 +0800

    搞定

--- cycleGradual/cycleGradual/ViewController.m
+++ cycleGradual/cycleGradual/ViewController.m
@@ -12,6 +12,7 @@
 @property (nonatomic,strong) CAShapeLayer *layer;
 @property (nonatomic,strong) CAShapeLayer *colorMaskLayer;
 @property (nonatomic,strong) CAShapeLayer *blueMaskLayer;
+@property (nonatomic,strong) UIView *blueBackView;
 
 @end
 
@@ -20,11 +21,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //[self createBlueView];
+    [self createBlueView];
     [self setupColorLayer];
-//    [self setupColorMaskLayer];
-//    [self setupBlueMaskLayer];
-//    [self setStoke];
+    [self setupColorMaskLayer];
+    [self setupBlueMaskLayer];
+    [self createSlider];
+    //[self setStoke];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -33,41 +35,47 @@
 -(void)createBlueView
 {
     UIView *blueView = [[UIView alloc] initWithFrame:CGRectMake(0, 100, 375, 375)];
+    _blueBackView = blueView;
+    blueView.backgroundColor = [UIColor colorWithRed:147.0/255.0 green:207.0/255.0 blue:248.0/255 alpha:1];
     [self.view addSubview:blueView];
 }
 
 -(void)setupColorLayer
 {
     CAShapeLayer *layer = [[CAShapeLayer alloc] init];
+    layer.frame = _blueBackView.bounds;
     _layer = layer;
-    layer.backgroundColor = [UIColor whiteColor].CGColor;
+    //layer.backgroundColor = [UIColor whiteColor].CGColor;
     
     CAGradientLayer *leftColorLayer = [[CAGradientLayer alloc] init];
     leftColorLayer.colors = @[(id)[UIColor yellowColor].CGColor,(id)[UIColor greenColor].CGColor];
     leftColorLayer.locations = @[@0.2,@0.7,@1.0];
-    leftColorLayer.frame = CGRectMake(0, 100, 375/2, 375);
+    //leftColorLayer.frame = CGRectMake(0, 0, 375/2, 375);
+    leftColorLayer.frame = CGRectMake(0, 0, _blueBackView.bounds.size.width/2, _blueBackView.bounds.size.height);
     [layer addSublayer:leftColorLayer];
     
     
     CAGradientLayer *rightColorLayer = [[CAGradientLayer alloc] init];
     rightColorLayer.colors = @[(id)[UIColor yellowColor].CGColor,(id)[UIColor redColor].CGColor];
     rightColorLayer.locations = @[@0.2,@0.7,@1.0];
-    rightColorLayer.frame = CGRectMake(375/2, 100, 375/2, 375);
+    //rightColorLayer.frame = CGRectMake(375/2, 0, 375/2, 375);
+    rightColorLayer.frame = CGRectMake(_blueBackView.bounds.size.width/2, 0, _blueBackView.bounds.size.width/2, _blueBackView.bounds.size.height);
     [layer addSublayer:rightColorLayer];
     
-    [self.view.layer addSublayer:layer];
+    [self.blueBackView.layer addSublayer:layer];
 }
 
 -(CAShapeLayer*)createCycleLayer
 {
     CAShapeLayer *layer = [[CAShapeLayer alloc] init];
-    
-    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(375/2, 100+375/2) radius:375/2 startAngle:-3/4*M_PI endAngle:-1/3*M_PI clockwise:YES];
+    //layer.backgroundColor = [UIColor orangeColor].CGColor;
+    layer.frame = _blueBackView.bounds;
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(_blueBackView.bounds.size.width/2,_blueBackView.bounds.size.height/2) radius:375/2.5 startAngle:-1.3*M_PI endAngle:0.3*M_PI clockwise:YES];
     layer.path = path.CGPath;
     layer.lineWidth = 20;
     layer.lineCap = kCALineCapRound;
     layer.fillColor = [UIColor clearColor].CGColor;
-    
+    layer.strokeColor = [UIColor blueColor].CGColor;
     return layer;
 }
 
@@ -79,20 +87,30 @@
     
     self.colorMaskLayer = [CAShapeLayer layer];
     self.colorMaskLayer = layer;
+    layer.strokeEnd = 0.3;
 }
 
 - (void)setupBlueMaskLayer {
     
     CAShapeLayer *layer = [self createCycleLayer];
-    self.layer.mask = layer;
+    //self.blueBackView.mask = layer;
+    self.blueBackView.layer.mask = layer;
     self.blueMaskLayer = layer;
-    
-    
 }
 
--(void)setStoke
+-(void)createSlider
 {
-    self.colorMaskLayer.strokeEnd = 0.5;
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(30, self.view.bounds.size.height-50, self.view.bounds.size.width-60, 20)];
+    slider.minimumValue = 0;
+    slider.maximumValue = 1;
+    slider.value = 0.5f;
+    [slider addTarget:self action:@selector(setStokeForSlider:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:slider];
+}
+
+-(void)setStokeForSlider:(UISlider*)slider
+{
+    self.colorMaskLayer.strokeEnd = slider.value;
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
